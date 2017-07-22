59607ae1bf732d169c40cf1b83f2d9f92d8080d8
Domagoj Šarić <dsaritz@gmail.com>
2017-07-22 20:57:00 +0200

    Fixed various build errors for targets other than ARM64.

--- nnpack.cmake
+++ nnpack.cmake
@@ -2,17 +2,23 @@
 set( nnpack_source ${CMAKE_CURRENT_LIST_DIR} )
 set( nnpack_interface_includes "${nnpack_source}/include" "${nnpack_source}/pthreadpool/include" )
 
+if ( WIN32 )
+  set( NNPACK_USE_MB_DISPATCH true )
+else()
+  option( NNPACK_USE_MB_DISPATCH "Make NNPACK use mb::sweat_shop for parallel jobs" true )
+endif()
+
 if ( MSVC AND NOT NNPACK_IN_MSVC_SUBPROJECT )
-  set( binary_dir "${CMAKE_BINARY_DIR}/nnpack" )
-  execute_process( COMMAND "${CMAKE_COMMAND}" -E make_directory "${binary_dir}" )
+  set( nnpack_binary_dir "${CMAKE_CURRENT_BINARY_DIR}/nnpack" )
+  execute_process( COMMAND "${CMAKE_COMMAND}" -E make_directory "${nnpack_binary_dir}" )
   execute_process(
     COMMAND "${CMAKE_COMMAND}"
+      -G ${CMAKE_GENERATOR}
       -T v141_clang_c2
       -DNNPACK_IN_MSVC_SUBPROJECT:BOOL=true
-      -DNNPACK_USE_MB_DISPATCH:BOOL=true
       -DMB_GLOBAL_ENABLE_TIMER:BOOL=false
       ${CMAKE_CURRENT_LIST_DIR}
-    WORKING_DIRECTORY "${binary_dir}"
+    WORKING_DIRECTORY "${nnpack_binary_dir}"
     ERROR_VARIABLE    stderr
     OUTPUT_VARIABLE   stdout
   )
@@ -20,12 +26,8 @@ if ( MSVC AND NOT NNPACK_IN_MSVC_SUBPROJECT )
   if ( stderr )
     message( FATAL_ERROR "NNPACK project creation failure:\n${stderr}" )
   endif()
-  include_external_msproject( NNPACK ${binary_dir}/NNPACK.vcxproj )
+  include_external_msproject( NNPACK "${nnpack_binary_dir}/NNPACK.vcxproj" )
   return()
-endif()
-
-if ( NOT WIN32 )
-  option( NNPACK_USE_MB_DISPATCH "Make NNPACK use mb::sweat_shop for parallel jobs" true )
 endif()
 
 #include( "${CMAKE_CURRENT_LIST_DIR}/../../core-utils/cmake-build/common_settings.cmake" )
@@ -40,27 +42,37 @@ file( GLOB_RECURSE nnpack_ref_sources    "${nnpack_source}/src/ref/*.c"        )
 file( GLOB_RECURSE nnpack_scalar_sources "${nnpack_source}/src/scalar/*.c"     )
 file( GLOB_RECURSE nnpack_x86_sources    "${nnpack_source}/src/x86_64-fma/*.c" )
 source_group( "Headers" FILES ${nnpack_common_headers} )
-if( NNPACK_USE_MB_DISPATCH )
-  set_source_files_properties( ${SRC_PATH}/pthreadpoolImpl.cpp PROPERTIES COMPILE_FLAGS "-DNN_NNPACK_USE_SWEATER=1" )
-else()
+if( NOT NNPACK_USE_MB_DISPATCH )
   set( pthread_pool_sources "${nnpack_source}/pthreadpool/src/threadpool-pthreads.c" )
 endif()
 list( APPEND nnpack_sources ${nnpack_common_headers} ${nnpack_common_sources} ${nnpack_scalar_sources} ${pthread_pool_sources} )
-if ( ANDROID OR iOS )
+if ( ANDROID )
+  if ( ANDROID_ABI MATCHES "arm" )
+    list( APPEND nnpack_sources ${nnpack_neon_sources} ${nnpack_psimd_sources} )
+  elseif( ANDROID_ABI MATCHES "x86" )
+    # no Android ABI or version guarantees AVX2 required by the x86 NNPACK backend
+    list( APPEND nnpack_sources ${nnpack_psimd_sources} )
+  endif()
+elseif ( iOS )
   list( APPEND nnpack_sources ${nnpack_neon_sources} ${nnpack_psimd_sources} )
 elseif( MSVC AND CMAKE_CXX_COMPILER_ID STREQUAL Clang )
   # neither x86 nor PSIMD backends work (see later comments for more info)
 else()
-  list( APPEND nnpack_sources ${nnpack_x86_sources} )
+  list( APPEND nnpack_sources ${nnpack_psimd_sources} ) # ${nnpack_x86_sources} don't want to run PeachPy or require AVX2
+  set( nnpack_explicit_backend PSIMD )
 endif()
 #set_source_files_properties( ${nnpack_sources} PROPERTIES LANGUAGE CXX )
 build_static_library( NNPACK ${nnpack_source} ${nnpack_sources} )
-target_include_directories( NNPACK PUBLIC ${nnpack_interface_includes} )
+target_include_directories( NNPACK PUBLIC  ${nnpack_interface_includes}   )
 target_include_directories( NNPACK PRIVATE ${nnpack_source}/src           )
 target_include_directories( NNPACK PRIVATE ${nnpack_source}/src/ref       )
 target_include_directories( NNPACK PRIVATE ${nnpack_source}/fp16/include  )
 target_include_directories( NNPACK PRIVATE ${nnpack_source}/FXdiv/include )
 target_include_directories( NNPACK PRIVATE ${nnpack_source}/psimd/include )
+
+if ( DEFINED nnpack_explicit_backend )
+  target_compile_definitions( NNPACK PRIVATE NNP_BACKEND_${nnpack_explicit_backend}=1 )
+endif()
 
 force_include_header( NNPACK "${nnpack_source}/src/c99.h" )
 if ( CMAKE_CXX_COMPILER_ID STREQUAL MSVC )
@@ -71,10 +83,21 @@ else()
 endif()
 
 if ( WIN32 )
-  target_compile_options( NNPACK PRIVATE
-    -DNOMINMAX -D__STDC__ # minmax macros
-    # -D__GNUC__ # for psimd.h
-    # -DNNP_BACKEND_PSIMD=1 # '_mm256_blendv_ps': Intrinsic not yet implemented (in MSVC14.1u2+Clang 3.8)
-    -DNNP_BACKEND_SCALAR=1 # nnpack\psimd\include\psimd.h(616): fatal error C1001: An internal error has occurred in the compiler (MSVC14.1u2)
+  target_compile_definitions( NNPACK PRIVATE
+    NOMINMAX __STDC__ # minmax macros
+    # __GNUC__ # for psimd.h
+    # NNP_BACKEND_PSIMD=1 # '_mm256_blendv_ps': Intrinsic not yet implemented (in MSVC14.1u2+Clang 3.8)
+    NNP_BACKEND_SCALAR=1 # nnpack\psimd\include\psimd.h(616): fatal error C1001: An internal error has occurred in the compiler (MSVC14.1u2)
+  )
+  target_compile_options( NNPACK PRIVATE -std=gnu11 -g2 -gdwarf-2 -mavx -ffast-math -fno-pic -fno-rtti -fno-exceptions $<$<CONFIG:RELEASE>:-O3 -fno-stack-protector> ) # -flto "fatal error LNK1136: invalid or corrupt file"
+elseif( ANDROID )
+  if ( ANDROID_ABI STREQUAL "armeabi-v7a" )
+    target_compile_options( NNPACK PRIVATE -mfpu=neon-fp16 )
+    target_link_libraries( NNPACK PUBLIC cpufeatures )
+  endif()
+elseif( iOS )
+  set_target_properties( NNPACK PROPERTIES
+    XCODE_ATTRIBUTE_OTHER_CFLAGS[arch=armv7] "$(OTHER_CFLAGS) -mfpu=neon-fp16"
+    XCODE_ATTRIBUTE_OTHER_CFLAGS[arch=armv7s] "$(OTHER_CFLAGS) -mfpu=neon-fp16"
   )
 endif()
--- src/c99.h
+++ src/c99.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#ifndef _MSC_VER
-    #define C99( expr ) expr
-    #define C99_FLOAT( hex, decimal ) hex
-#else
+#if defined( _MSC_VER ) && !defined( __clang__ )
     #define C99( expr )
     #define C99_FLOAT( hex, decimal ) decimal
     #define restrict /*...mrmlj...messes with __declspec(restrict)...__restrict*/
+#else
+    #define C99( expr ) expr
+    #define C99_FLOAT( hex, decimal ) hex
 #endif // _MSC_VER
